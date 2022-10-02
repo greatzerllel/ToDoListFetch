c1c27204f642c0f1153c22266aecc426df9e0911
cgabrielP <cgabriel2303@gmail.com>
2022-10-02 22:27:17 +0000

    POST

--- src/js/component/Form.js
+++ src/js/component/Form.js
@@ -1,20 +1,43 @@
 import React from "react";
 
+const Form = ({ setInputText, setTodos, todos, inputText }) => {
+  
 
-const Form = ({ setInputText, setTodos, todos, inputText,setStatus }) => {
   const inputTextHandler = (e) => {
     setInputText(e.target.value);
     console.log(inputText);
   };
   const submitTodoHandler = (e) => {
     e.preventDefault();
-    if(inputText == ''){
-      alert('Introduce tu task')
-    }else
-    {setTodos([
-      ...todos,
-      { text: inputText, completed: false, id: Math.random() * 1000 },
-    ]);}
+    if (inputText == "") {
+      alert("Introduce tu task");
+    } else {
+      setTodos([
+        ...todos,
+        { text: inputText, completed: false, id: Math.random() * 1000 },
+      ])
+      fetch('https://assets.breatheco.de/apis/fake/todos/user/pruebanumero116', {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json"
+    },
+   /*  body: JSON.stringify(todos) */
+  })
+  .then(resp => {
+      console.log(resp.ok); // Será true (verdad) si la respuesta es exitosa.
+      console.log(resp.status); // el código de estado = 200 o código = 400 etc.
+       // Intentará devolver el resultado exacto como cadena (string)
+      return resp.json(); // (regresa una promesa) will try to parse the result as json as return a promise that you can .then for results
+  })
+  .then(data => {
+      //Aquí es donde debe comenzar tu código después de que finalice la búsqueda
+      console.log(data); //esto imprimirá en la consola el objeto exacto recibido del servidor
+  })
+  .catch(error => {
+      //manejo de errores
+      console.error(error);
+  });;
+    }
     setInputText("");
   };
 
